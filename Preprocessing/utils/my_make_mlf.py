32e27d2504716085686f0f76a9a4fc081fd5832b
WHH <33870431+XiaoshanHsj@users.noreply.github.com>
2021-02-01 21:28:41 +0800

    Create my_make_mlf.py

---
+++ Preprocessing/utils/my_make_mlf.py
@@ -0,0 +1,38 @@
+import sys
+import os
+import re
+
+# sys.argv[1]: plp scp file (with time label)
+# sys.argv[2]: output mlf file path
+
+train_scp = sys.argv[1]
+out_mlf_path = sys.argv[2]
+
+raw_path = "/project_bdda3/bdda/sjhu/Data/torgo/"
+
+with open(out_mlf_path, 'w') as f_w:
+    f_w.write("#!MLF!#\n")
+    with open(train_scp) as f_r:        
+        for line in f_r:
+            mlf_path = line.strip().split('=')[0].split('.')[0]
+            try:
+                _, _, s, _, index = mlf_path.split('-')
+            except:
+                print(line.strip())
+            s = s[s.rfind("X")+1:]
+            sp = s[0:2]
+            sess = s[2:]
+            index = index.split('_')[1].lstrip('X')
+            
+            pro_path = raw_path + sp + "/" + sess + "/prompts/" + index + ".txt"
+            with open(pro_path, "r") as f:
+                utterance = f.readline()
+                if not utterance[-1].isalpha():
+                    utterance = utterance[:-1]
+                
+                mlf_full_path = "\"*/" + mlf_path + ".lab\""
+                f_w.write(mlf_full_path + '\n')
+                words = utterance.split()
+                for word in words:  
+                    f_w.write(word + '\n')
+                f_w.write('.\n')

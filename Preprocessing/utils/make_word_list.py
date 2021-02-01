11a397a935e8c91f367087d2f77d2623ad89b64e
WHH <33870431+XiaoshanHsj@users.noreply.github.com>
2021-02-01 20:47:54 +0800

    Update make_word_list.py

--- Preprocessing/utils/make_word_list.py
+++ Preprocessing/utils/make_word_list.py
@@ -14,12 +14,22 @@ for sp in speakers:
         sess = "Session" + name.split("_")[1][-1]
         num = name.split("_")[2]
         pro_path = raw_path + sp + "/" + sess + "/prompts/" + num + ".txt"
-        with open(pro_path, 'r') as f:
+        wav_abs_path = wav_path + "/" + wav
+        pos_abs_path = ori_path + sp + "/pos/" + name + ".pos"
+        # 如果对应的prompts不存在也把对应的wav和pos删去
+        if not os.path.exists(pro_path):
+            try:
+                os.remove(wav_abs_path)
+                print("删去了" + wav_abs_path)
+                os.remove(pos_abs_path)
+                print("删去了" + pos_abs_path)
+            except:
+                print(pro_path)
+        else:
+            with open(pro_path, 'r') as f:
             line = f.readline()
             if line.find(".jpg") != -1 or line.find("[") != -1 or line.find("]") != -1:
                 # 此时我们需要将对应的.wav和.pos删去
-                wav_abs_path = wav_path + "/" + wav
-                pos_abs_path = ori_path + sp + "/pos/" + name + ".pos"
                 try:
                     os.remove(wav_abs_path)
                     print("删去了" + wav_abs_path)

665c02981990b4ac0f3aa4e007c671dc01d67723
WHH <33870431+XiaoshanHsj@users.noreply.github.com>
2021-02-02 10:52:49 +0800

    2

---
+++ Preprocessing/utils/make_dict.py
@@ -0,0 +1,19 @@
+import os,sys
+
+dict_path = "/project_bdda3/bdda/sjhu/Data/Raw_data/torgo/data/dict/lexicon.txt"
+save_path = "/project_bdda3/bdda/sjhu/Data/Raw_data/torgo/convert/lib/dicts/"
+filename = "train.pi_ti.dct"
+
+with open(save_path + filename, "w") as f_w:
+    with open(dict_path , "r") as f_r:
+        for line in f_r:
+            word = line.split()[0]
+            phones = line.split()[1:]
+            if word.find("SIL") != -1:
+                continue
+            else:
+                new_line = word + "\t\t"
+                for phone in phones:
+                    new_line = new_line + phone + "\t"
+                new_line = new_line[:-1]
+                f_w.write(new_line + "\n")
